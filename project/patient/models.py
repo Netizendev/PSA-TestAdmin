7408aa1d43ab6c27cf5c5238f658757fdba68db2
Netizendev <benjamin.nyhlen@gmail.com>
2021-04-20 14:43:02 +0200

    model update

--- project/patient/models.py
+++ project/patient/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Patient(models.Model):
 	name = models.CharField(max_length=150)
 	ssn = models.CharField(max_length=100)
+	created = models.DateTimeField(auto_now_add=True)
+	
+	def __str__(self):
+		return self.ssn
