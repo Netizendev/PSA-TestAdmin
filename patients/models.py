fd4522dff3a0783b01ddc97ddab3541844d54122
Netizendev <benjamin.nyhlen@gmail.com>
2021-04-20 12:08:14 +0200

    patient model updated

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,10 @@
-from django.db import models
-
 # Create your models here.
+
+class Patients(models.Model):
+	name = models.CharField(max_length=200)
+    ssn = models.CharField(max_length=200)
+    frequency = models.CharField(max_length=200)
+	created = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.ssn
